a88bcc33092270c8402b95db6d4954477070ee49
SolitaryRehman <ariaz.bscs25seecs@seecs.edu.pk>
2026-09-08 15:09:04 +0500

    Lab 01 Task: 02

---
+++ Labs/Lab 1/task_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numbers [5];
+    int total = 0;
+
+    cout << "Enter 5 numbers seperated by spaces: " ;
+    
+    for ( int i = 0 ; i < 5 ; i++)
+    {
+        cin >> numbers[i];
+    }
+
+    for (int i = 0 ; i < 5 ; i++)
+    {
+        total += numbers[i]; 
+    }
+
+    cout << endl << "Total = " << total << endl;
+}
